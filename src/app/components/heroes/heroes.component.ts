2dcef05b97ae7bc235063e4826476f3c352b7791
Alex <alex45v90@gmail.com>
2018-11-08 02:41:10 +0300

    Edit receive data to heroes component with help Hero service

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { Hero } from '../../models/';
-import { HEROES } from '../../mocks/';
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../../models';
+import {HeroService} from '../../services/hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent {
-
-  heroes: Hero[] = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
   public selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
