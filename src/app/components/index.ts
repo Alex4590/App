bef33147aa08eb37defd728133b46e6104cd49c5
Alex <alex45v90@gmail.com>
2018-11-11 21:37:07 +0300

    Add hero-search component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,3 +2,4 @@ export { HeroesComponent} from './heroes/heroes.component';
 export { HeroDetailComponent} from './hero-detail/hero-detail.component';
 export { MessagesComponent } from './messages/messages.component';
 export { DashboardComponent } from './dashboard/dashboard.component';
+export { HeroSearchComponent } from './hero-search/hero-search.component';
